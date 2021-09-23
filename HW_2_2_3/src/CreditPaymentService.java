acafa0cd8a6280633e98c494937a0b50e6a836e1
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2021-09-23 21:51:55 +0300

    Add new changes

--- HW_2_2_3/src/CreditPaymentService.java
+++ HW_2_2_3/src/CreditPaymentService.java
@@ -1,14 +1,14 @@
 public class CreditPaymentService {
-    public int calculate(int year) {
+    public int calculate(int year, int sum, double percent) {
         int month = year * 12;
-        double percent = 9.99;
+        //double percent = 9.99;
         double mps = percent / 12 / 100;
         //mps - месячная процентная ставка
         double mps2 = Math.pow(1 + mps, month);
         //mps2 - (1+месячная процентная ставка)^(количество платежей)
         double ka = (mps * mps2) / (mps2 - 1);
         // ka - коэффициент аннуитета
-        int sum = 1_000_000;
+        //int sum = 1_000_000;
         int payment = (int) (sum * ka);
         return payment;
     }
--- HW_2_2_3/src/Main.java
+++ HW_2_2_3/src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        int payment1 = service.calculate(1);
+        int payment1 = service.calculate(1, 1_000_000, 9.99);
         System.out.println(payment1);
 
-        int payment2 = service.calculate(2);
+        int payment2 = service.calculate(2, 1_000_000, 9.99);
         System.out.println(payment2);
 
-        int payment3 = service.calculate(3);
+        int payment3 = service.calculate(3, 1_000_000, 9.99);
         System.out.println(payment3);
 
     }
